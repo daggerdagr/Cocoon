90c2be98d89d0b909b561844738c5accc4672ba2
Regina Ongowarsito <regina.ongowarsito@gmail.com>
2016-06-07 04:14:56 -0700

    first commit

---
+++ Assets/CamControls.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControls : MonoBehaviour {
+
+	public GameObject cursor;
+	private Vector3 cursorcoord;
+	public float dist;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cursorcoord = this.transform.forward;
+		cursorcoord = Vector3.Scale (cursorcoord, new Vector3 (dist, dist, dist));
+		cursor.transform.position = cursorcoord;
+
+		//Debug.Log (transform.forward);
+		//Debug.Log (Input.GetAxis("Mouse X"));
+
+		//cam controoools
+		//w/ mouse:
+		//float moveLR = Input.GetAxis("Mouse X");
+		//float moveUD = Input.GetAxis("Mouse Y");
+		float moveLR = Input.GetAxis("Horizontal");
+		float moveUD = Input.GetAxis("Vertical");
+		this.transform.Rotate (new Vector3 (-1 * moveUD, moveLR, 0), Space.World);
+		Debug.Log (moveUD);
+		Debug.Log (moveLR);
+	}
+}
---
+++ Assets/LineDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineDrawer : MonoBehaviour {
+
+	public GameObject cursor;
+	private LineRenderer line;
+	private int num = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//line = GetComponent<LineRenderer> ();
+		Debug.Log (num);
+		num += 1;
+	}
+}
