5f2aebb69804678b8553cfcb470d25c404387508
Regina Ongowarsito <regina.ongowarsito@gmail.com>
2016-06-08 05:04:38 -0700

    made camera move, made lines break, yayayayaya. except holy shit the code's messy as all hell. also, you need to start looking into ui because you have 3 modes to switch around w/ and the only reason why you have intuition for it is because, whaddya know, you built it

--- Assets/CamControls.cs
+++ Assets/CamControls.cs
@@ -1,35 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CamControls : MonoBehaviour {
 
 	public GameObject cursor;
 	private Vector3 cursorcoord;
 	public float dist;
+	//private int[] modes = new int[0, 1, 2];
+	private int mode = 0;
+	public Text modeStatus;
+	public GameObject lineControl;
+	private Transform transf;
 
 	// Use this for initialization
 	void Start () {
+		//OVRTouchpad.Create ();
+		OVRTouchpad.TouchHandler += HandleTouchHandler;
+		transf = transform.parent; 
 	}
-	
+
+	void HandleTouchHandler (object sender, System.EventArgs e) {
+		OVRTouchpad.TouchArgs touchArgs = (OVRTouchpad.TouchArgs)e;
+		if (mode == 1) {
+			if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Right) {
+				dist += (float) 0.5;
+			} else if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Left) {
+				if (dist > 0) {
+					dist -= (float) 0.5;
+				}
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			mode = (mode + 1) % 3;
+			lineControl.SendMessage ("ModeChange");
+		}
+
+		cursor.SetActive (true);
+		if (mode == 0) {
+			modeStatus.text = "Draw Mode";
+		} else if (mode == 1) {
+			modeStatus.text = "Tool Mode";
+		} else {
+			modeStatus.text = "Move Mode";
+			cursor.SetActive (false);
+		}
+
+		if (mode == 2 & Input.GetKey (KeyCode.Mouse0)) {
+			transf.transform.position += transform.forward * Time.deltaTime * 5;
+		}
+			
+
 		cursorcoord = this.transform.forward;
 		cursorcoord = Vector3.Scale (cursorcoord, new Vector3 (dist, dist, dist));
-		cursor.transform.position = cursorcoord;
+		cursor.transform.position = this.transform.position + cursorcoord;
 
-		dist += Input.GetAxis ("Mouse Y") * ((float) 0.1);
-		Debug.Log ((float)0.1);
+		//if (Input.GetAxis ("Mouse Y") > 0) {
+		//	dist += (float) 0.5;
+		//} else if (Input.GetAxis ("Mouse Y") < 0) {
+		//	if (dist > 0) {
+		//		dist -= (float) 0.5;
+		//	}
+		//}
 
-		//Debug.Log (transform.forward);
+		//dist += Input.GetAxis ("Mouse Y") * ((float) 0.1);
+		//Debug.Log ((float)0.1);
+
+		Debug.Log (transform.forward);
 		//Debug.Log (Input.GetAxis("Mouse X"));
 
 		//cam controoools
 		//w/ mouse:
 		//float moveLR = Input.GetAxis("Mouse X");
 		//float moveUD = Input.GetAxis("Mouse Y");
-		//float moveLR = Input.GetAxis("Horizontal");
-		//float moveUD = Input.GetAxis("Vertical");
-		//this.transform.Rotate (new Vector3 (-1 * moveUD, moveLR, 0), Space.World);
+		float moveLR = Input.GetAxis("Horizontal");
+		float moveUD = Input.GetAxis("Vertical");
+		this.transform.Rotate (new Vector3 (-1 * moveUD, 0, 0));
+		this.transform.Rotate (new Vector3 (0, moveLR, 0), Space.World);
 		//Debug.Log (moveUD);
 		//Debug.Log (moveLR);
 	}
--- Assets/LineDrawer.cs
+++ Assets/LineDrawer.cs
@@ -6,20 +6,44 @@ public class LineDrawer : MonoBehaviour {
 	public GameObject cursor;
 	private LineRenderer line;
 	private int num = 0;
+	private bool lineOn;
+	public GameObject lineOrig;
+	private GameObject currLine;
+	private int mode = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+	}
+
+	void ModeChange() {
+		mode = (mode + 1) % 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.Mouse0)) {
-			line = GetComponent<LineRenderer> ();
-			line.SetVertexCount (num + 1);
-			line.SetPosition (num, cursor.transform.position);
-			Debug.Log (num);
-			num += 1;
+		//downclick = false;
+		//Object.Instantiate (this);
+		//Debug.Log(Input.GetKey(KeyCode.A));
+		if (mode == 0) {
+			if (lineOn == false & Input.GetKey (KeyCode.Mouse0)) {
+				lineOn = true;
+				num = 0;
+				Debug.Log ("hit");
+				currLine = Object.Instantiate (lineOrig);
+				currLine.transform.parent = lineOrig.transform.parent; 
+			} else if (lineOn == true & !Input.GetKey (KeyCode.Mouse0)) {
+				lineOn = false;
+				Debug.Log ("done");
+			}
+
+			//Debug.Log (lineOn);
+			if (Input.GetKey (KeyCode.Mouse0)) {
+				line = currLine.GetComponent<LineRenderer> ();
+				line.SetVertexCount (num + 1);
+				line.SetPosition (num, cursor.transform.position);
+				//Debug.Log (num);
+				num += 1; 
+			}	
 		}
 	}
 }
